9287f80bd4d535dca041b19f2a2f897928bea0e5
Hemant Soni <sonihemant111@gmail.com>
2021-05-07 17:27:20 +0530

    Added print statement

--- WeatherInfo/Lifecycle/AppDelegate.swift
+++ WeatherInfo/Lifecycle/AppDelegate.swift
@@ -22,6 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             self.fetchCityDataFromJSON()
         }
         dbManager.setDefaultCity()
+        print("Inside Appdelegate class")
         return true
     }
     
